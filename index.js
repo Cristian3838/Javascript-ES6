9cc56e940bd43aa7b5efcddf2ac3b4313c1b551b
Cristian3838 <crisitinajonas@vivointernetdiscada.com.br>
2020-06-18 04:16:30 -0300

    Atualização 01

---
+++ index.js
@@ -0,0 +1,59 @@
+//Reccebe valor da função
+/*function log(value) {
+    console.log(value);
+}
+
+log("Testando java");*/
+
+
+/*var log = function(value) {
+    console.log(value);
+
+}
+
+log("Testando Java")*/
+
+
+/*Função de soma
+
+var sun = function(a, b) {
+    return a + b;
+};
+
+console.log(sun(5, 5));*/
+
+
+/*Arrow function
+
+var sum = (a, b) => a + b;
+console.log(sum(5, 100));*/
+
+/*
+var createObj = () => ({ teste: 123 });
+console.log(createObj());*/
+
+/*
+function Car() {
+    this.Objeto = "bar";
+}
+console.log(new Car());*/
+
+
+//Arrow functions
+
+/*
+var obj = {
+    showContext: function showContext() {
+
+        setTimeout(() => {
+            this.log("After 1000ms");
+        }, 1000);
+    },
+
+
+    log: function log(value) {
+        console.log(value);
+    }
+};
+
+obj.showContext();*/
