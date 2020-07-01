d2a65c9bcb1ca14a8046f58fb535cd412dfa3034
Cristian3838 <crisitinajonas@vivointernetdiscada.com.br>
2020-07-01 17:30:58 -0300

    Atualização03

--- test/math.spec.js
+++ test/math.spec.js
@@ -3,11 +3,6 @@ const Math = require("../src/math.js");
 describe("Math class", function() {
     it("Sum two numbers", function() {
         const math = new Math();
-
-        try {
-            assert.equal(math.sum(5, 5), 10);
-        } catch (err) {
-            console.log(err);
-        }
+        assert.equal(math.sum(5, 5), 10);
     });
 });
